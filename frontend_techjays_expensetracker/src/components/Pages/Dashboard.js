1868cf3cc7d82b810803cf49eb413e25d85a5ebd
ajmal4414 <122264907+ajmal4414@users.noreply.github.com>
2023-12-02 17:52:26 +0530

    Update Dashboard.js

--- frontend_techjays_expensetracker/src/components/Pages/Dashboard.js
+++ frontend_techjays_expensetracker/src/components/Pages/Dashboard.js
@@ -1,10 +1,9 @@
 import React from 'react'
-// import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import GooglePieChart from '../Charts/PieChart';
 import GoogleBarChart from '../Charts/GoogleBarChart'
 import Table from '../Tables/Table';
-// import BarChart from './BarChart/BarChart';
+
 
 
 
