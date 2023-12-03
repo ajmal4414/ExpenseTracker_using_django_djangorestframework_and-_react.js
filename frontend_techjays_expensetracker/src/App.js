e28d5c67ff8e66164df79d3dfdb49d3821aad1a2
ajmal4414 <122264907+ajmal4414@users.noreply.github.com>
2023-12-03 14:09:24 +0530

    Update App.js

--- frontend_techjays_expensetracker/src/App.js
+++ frontend_techjays_expensetracker/src/App.js
@@ -10,7 +10,6 @@ import styled from "styled-components";
 import AddExpense from './components/Pages/AddExpense'
 import ExpenseHistoryPage from './components/Pages/ExpenseHistory'
 import EditExpensePage from './components/Pages/EditExpense'
-// import ReportsPage from './components/BarChart/BarChart'
 import GoogleBarChart from './components/Charts/GoogleBarChart'
 import GooglePieChart from './components/Charts/PieChart'
 import Home from './components/Pages/Home'
@@ -18,15 +17,11 @@ import './App.css'
 import ReportsPage from './components/Pages/Report'
 import AboutUsPage from './components/Pages/AboutUs'
 import PasswordReset from './components/Auth/PasswordReset';
-
 import { Outlet } from "react-router-dom";
 import LandingPage from './components/LandingPage/LandingPage'
 import AllSetPage from './components/Auth/AllSetPage'
 import { Navigate } from 'react-router-dom';
 import AddExpensePage from './components/Pages/AddExpense'
-
-// import AddExpensePage from './components/AddExpense'
-// import 
 
 const Container = styled.div`
     width: 100%;
@@ -138,76 +133,30 @@ useEffect(() => {
       <SidebarContainer>
         <Sidebars>
       <Sidebar/>
-
-      
       </Sidebars>
       </SidebarContainer>
       <MainBoxContainer>
-      
-      
               <MainBox>
-
-              <Outlet />
-              
+              <Outlet /> 
                 {!AccessToken ? (
                   <LandingPage />
                 ) : (
-
-  
-
-    <Routes>
-    
-    <Route exact path="/password-reset" Component={PasswordReset} />
-    <Route path="/" element={<AllSetPage />} />
-    <Route path="/home" element={<Home />} />
- 
-     <Route exact path ='/dashboard' Component={Dashboard}/> 
-
-     <Route path="/Add-expense" element={<AddExpensePage />} />
-       <Route path="/Expense-History" element={<ExpenseHistoryPage />} />
-      <Route path="Edit_Expense/:id" element={<EditExpensePage />} />
+        <Routes> 
+        <Route exact path="/password-reset" Component={PasswordReset} />
+        <Route path="/" element={<AllSetPage />} />
+        <Route path="/home" element={<Home />} />
+        <Route exact path ='/dashboard' Component={Dashboard}/> 
+        <Route path="/Add-expense" element={<AddExpensePage />} />
+        <Route path="/Expense-History" element={<ExpenseHistoryPage />} />
+        <Route path="Edit_Expense/:id" element={<EditExpensePage />} />
         <Route path="/Report" element={<ReportsPage />} />
         <Route path="/barchart" element={<GoogleBarChart />} />
-        {/* <Route path="/helppage" element={<Hel />} /> */}
-        {/* <Route path="/Dp" element={<DpView />} /> */}
-
-
-
-
-           {/* <Route path="/password-reset" element={<PasswordReset/>} /> */}
-    
-    {/* <Route path="/" element={<LandingPage />} /> */}
-    {/* <Route path ='' Component={Login}/>  */}
-    {/* <Route exact path ='/Sign-in' Component={Signup}/> */}
-    {/* <Route path='/' element={NavBar} ></Route> */}
-    {/* <Route exact path ='/home' Component={Home}/>  */}
-        </Routes>
-                )}
-   
-    {/* <Route path ='/Sign-in' Component={Login}/>  */}
-
-{/* 
-    <Route path = '/Add-expense' Component={AddExpense}/>
-    <Route path = '/Expense-History' Component={ExpenseHistoryPage}/>
-     <Route path='/Edit-Expense/' Component={EditExpensePage}/>
-    <Route path='/Bar-chart' Component={GoogleBarChart}/>
-    <Route path='/Google-pie-chart' Component={GooglePieChart}/> 
-    <Route path='/Report' Component={ReportsPage}/> 
-    </Routes> */}
-
-    </MainBox>
+            </MainBox>
             </MainBoxContainer>
           </Wrapper>
         </ContainerSub>
-      </Container>
-      
+      </Container> 
     </Router>
-
-    /* // <div>
-    // <Signup/>
-    // <Login/>
-    </div> */
-
 
   );
 }
@@ -236,18 +185,3 @@ function ProtectedRoutes() {
 
 export default App
 
-
-// function App() {
-//   return (
-//     <Router>
-//       <Routes>
-//         <Route path='/' element={<NavBar />} />
-//         <Route path='/dashboard' element={<Dashboard />} />
-//         <Route path='/Sign-up' element={<Signup />} />
-//         <Route path='/Sign-in' element={<Login />} />
-//       </Routes>
-//     </Router>
-//   );
-// }
-
-// export default App;
